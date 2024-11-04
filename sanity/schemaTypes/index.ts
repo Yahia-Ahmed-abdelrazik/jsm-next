62c9f2d1bd2ed910f6eda71932182a7eea35db2d
Yahia-Ahmed-abdelrazik <otakayahia@gmail.com>
2024-11-04 21:01:20 +0200

    add-author-starture-document

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { author } from "@/sanity/schemaTypes/author";
+import { startUp } from "@/sanity/schemaTypes/startuUp";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startUp],
+};
